ba39ed5d0d71114bc9fc9e7656110a1043ccb2b0
lucaskrespi <lucas.krespi@gmail.com>
2021-09-27 21:38:06 -0400

    Buttons and scenes added

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    private Scene currentScene;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentScene = SceneManager.GetActiveScene();
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+    }
+    public void PreviousButton()
+    {
+        SceneManager.LoadScene(currentScene.buildIndex - 1);
+    }
+    public void InstructionsButton()
+    {
+        Debug.Log("INSTRUCTIONS");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
